dcd160ba1c680912a5c2ad14b22f8e6cdaf83221
staight <chen27508959@gmail.com>
2018-08-02 21:01:47 +0800

    update: problem title

--- 1. Two Sum.go
+++ 1. Two Sum.go
@@ -1,3 +1,18 @@
+/*
+1. Two Sum
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
 //it is a brute force approach
 func twoSum(nums []int, target int) []int {
 	nLen:=len(nums)
--- 4. Median of Two Sorted Arrays.go
+++ 4. Median of Two Sorted Arrays.go
@@ -1,3 +1,29 @@
+/*
+4. Median of Two Sorted Arrays
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+ 
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+*/
+
 //merge two sorted arrays as a big sorted array, and then find its median
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	//try to merge the two arrays: nums1 and nums2
